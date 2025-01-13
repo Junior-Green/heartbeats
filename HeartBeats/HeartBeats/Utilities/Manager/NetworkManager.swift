f669ba5d42190045732649e23bcea937c51d2246
Junior Green <juniorgreen9185@hotmail.com>
2025-01-13 17:13:15 -0500

    created backend workspace, implemented udsocket package, and server object model and initialization

--- HeartBeats/HeartBeats/Utilities/Manager/NetworkManager.swift
+++ HeartBeats/HeartBeats/Utilities/Manager/NetworkManager.swift
@@ -5,31 +5,32 @@
 //  Created by Junior Green on 2025-01-07.
 //
 
-import SwiftUI
-
-final class NetworkManager: UDSClientDelegate {
+final class NetworkManager {
   private let logger = Logger.shared
   private let client: UDSClient = .init(socketUrl: UDSClient.serviceUrl())
+  private let delegate: UDSClientDelegate?
   
   static let shared = NetworkManager()
   
   private init() {
-    client.delegate = self
+    self.delegate = UDSDelegate()
+    client.delegate = delegate
     
-    do {
-      try client.start()
-    }
-    catch {
-      logger.log(error.localizedDescription)
-    }
+//    do {
+//      try client.start()
+//    }
+//    catch {
+//      logger.log(error.localizedDescription)
+//    }
   }
-  
-  deinit {
-    self.client.stop()
-  }
-  
+
+//
+//  deinit {
+//    self.client.stop()
+//  }
   
   // MARK: - API Methods
+
   func getServer(hostname: String) throws -> Server {
     throw HBError.DefaultError
   }
@@ -44,20 +45,5 @@ final class NetworkManager: UDSClientDelegate {
   
   func getAllServers() throws -> [Server] {
     throw HBError.DefaultError
-  }
-  
-  // MARK: - Delegate Methods
-  func handleSocketClientMsgDict(_ aDict: [AnyHashable: AnyHashable]?, client: UDSClient?, error: (any Error)?) {
-    if error != nil {
-      logger.logError(error!)
-    }
-  }
-  
-  func handleSocketClientDisconnect(_ client: UDSClient?) {
-    logger.log("Client has disconnected")
-  }
-  
-  func handleSocketServerDisconnect(_ client: UDSClient?) {
-    logger.log("Client has disconnected")
   }
 }
